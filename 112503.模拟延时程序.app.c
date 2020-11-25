8270721cbff03ff7a0f294796525d321101ef147
qq1990716 <74294827+qq1990716@users.noreply.github.com>
2020-11-25 16:50:06 +0800

    延时钥匙

---
+++ 112503.模拟延时程序.app.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<windows.h>
+void main(){
+	printf("男：穿秋裤没\n");
+	Sleep(1000);
+	printf("女：没有\n");
+	Sleep(500);
+	printf("男：我今天穿了，贼暖，贼舒服\n");
+	Sleep(800);
+	printf("女：我明天也要穿\n");
+	Sleep(800);
+	printf("男：你个小猪猪~~\n");
+	Sleep(800);
+	printf("女：？？？？\n");
+	Sleep(800);
+	printf("女：你个臭猪\n");
+}
